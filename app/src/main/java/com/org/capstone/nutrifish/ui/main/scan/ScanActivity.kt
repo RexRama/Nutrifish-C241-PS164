4921658bdca23e9b9eba1a56d9db0de18466e927
RaidRama <raidhan.rama.11@gmail.com>
2024-06-03 22:02:12 +0700

    UI Update
    UI Update:
    - Add scan page
    - Change welcome page

--- app/src/main/java/com/org/capstone/nutrifish/ui/main/scan/ScanActivity.kt
+++ app/src/main/java/com/org/capstone/nutrifish/ui/main/scan/ScanActivity.kt
@@ -1,34 +1,21 @@
-package com.org.capstone.nutrifish.ui.auth.login
+package com.org.capstone.nutrifish.ui.main.scan
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.org.capstone.nutrifish.R
-import com.org.capstone.nutrifish.databinding.ActivityLoginBinding
-import com.org.capstone.nutrifish.utils.Utils
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+class ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_scan)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        setButtonAction()
-    }
-
-    private fun setButtonAction() {
-        val toRegister = binding.btLoginToRegister
-
-        toRegister.setOnClickListener {
-            Utils().toRegister(this)
-            finish()
         }
     }
 }
