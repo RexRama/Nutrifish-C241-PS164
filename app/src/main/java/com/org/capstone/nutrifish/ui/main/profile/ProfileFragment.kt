527ac13398f15cf5eb9cb6249f7d3773f8c4e499
RaidRama <raidhan.rama.11@gmail.com>
2024-06-09 06:23:49 +0700

    Machine Learning Model Implementation
    ML Model Implementation:
    - Implementation ML for image classification in Scan Fragment

--- app/src/main/java/com/org/capstone/nutrifish/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/org/capstone/nutrifish/ui/main/profile/ProfileFragment.kt
@@ -1,12 +1,10 @@
 package com.org.capstone.nutrifish.ui.main.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.org.capstone.nutrifish.R
-import com.org.capstone.nutrifish.databinding.FragmentHomeBinding
+import androidx.fragment.app.Fragment
 import com.org.capstone.nutrifish.databinding.FragmentProfileBinding
 
 
