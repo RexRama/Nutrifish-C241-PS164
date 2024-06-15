78c6cc6b5bffdda88926b45613f4c9223dacbee6
RaidRama <raidhan.rama.11@gmail.com>
2024-06-15 22:52:05 +0700

    UI Updates
    UI Update:
    - Add back button for detail page
    - Hide bottom navbar on some page
    - Fix UI Issues

--- app/src/main/java/com/org/capstone/nutrifish/data/remote/api/ApiService.kt
+++ app/src/main/java/com/org/capstone/nutrifish/data/remote/api/ApiService.kt
@@ -1,15 +1,23 @@
 package com.org.capstone.nutrifish.data.remote.api
 
+import android.content.ClipDescription
 import com.org.capstone.nutrifish.data.remote.model.LoginModel
 import com.org.capstone.nutrifish.data.remote.model.RegisterModel
 import com.org.capstone.nutrifish.data.remote.response.LoginResponse
 import com.org.capstone.nutrifish.data.remote.response.MyPostResponse
 import com.org.capstone.nutrifish.data.remote.response.RegisterResponse
 import com.org.capstone.nutrifish.data.remote.response.StoryResponse
+import com.org.capstone.nutrifish.data.remote.response.UploadStoryResponse
+import com.org.capstone.nutrifish.data.remote.response.VerifyGoogleTokenResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -41,4 +49,23 @@ interface ApiService {
         @Query("page") page: Int,
         @Query("size") size: Int
     ): MyPostResponse
+
+    //Upload Story
+    @Multipart
+    @POST("stories")
+    fun uploadStory(
+        @Header("Authorization")token: String,
+        @Part photo: MultipartBody.Part,
+        @Part("storyTitle") storyTitle: RequestBody,
+        @Part("storyDescription") storyDescription: RequestBody,
+        @Part("lat") lat: Double?,
+        @Part("lon") lon: Double?,
+    ) : Call<UploadStoryResponse>
+
+    @POST("verify-google-token")
+    fun verifyGoogleToken(
+        @Body idToken: RequestBody
+    ): Call<VerifyGoogleTokenResponse>
+
+
 }
