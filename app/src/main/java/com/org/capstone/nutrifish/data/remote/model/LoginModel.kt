708224c37bbf19457d537a84d962f47b9bbc8833
Raihan Ramadhan <90739806+RexRama@users.noreply.github.com>
2024-06-16 20:22:18 +0700

    Delete app directory

--- app/src/main/java/com/org/capstone/nutrifish/data/remote/model/LoginModel.kt
+++
@@ -1,6 +0,0 @@
-package com.org.capstone.nutrifish.data.remote.model
-
-data class LoginModel(
-    val email: String,
-    val password: String
-)
--- app/src/main/java/com/org/capstone/nutrifish/data/remote/model/RegisterModel.kt
+++
@@ -1,8 +0,0 @@
-package com.org.capstone.nutrifish.data.remote.model
-
-data class RegisterModel(
-    val name: String,
-    val username: String,
-    val email: String,
-    val password: String
-)
